caf632508f77327d3e1af832a15563b1559af855
19cmogush <cmogush@usc.edu>
2022-11-11 12:42:13 -0800

    airtable logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import Airtable, { Base } from "airtable";
 // import cors from "cors"; // for CORS setup, usage: app.use(cors());
 
 const app = express();
@@ -15,8 +16,45 @@ app.get('/api', (req: Request, res: Response) => {
 
 
 app.post('/api/sendToAirtable', (req: Request, res: Response) => {
-  res.json({"msg": "hello from vercel!"});
+  const Airtable = require("airtable");
+  const base = new Airtable({apiKey: "keyCIWaCgtovfq4fn"}).base("appiWMugRGpJ4p3KS");
+  if (req.body.startFireCamp == "") {
+    req.body.startFireCamp = null;
+    req.body.endFireCamp = null;
+  }
+  base("Table_1").create(
+    {
+      "fields": {
+        "Full_Name": req.body.fullName,
+        "Phone": req.body.phone,
+        "Email": req.body.email,
+        "Date_of_Birth": req.body.dob,
+        "Crime_Convicted_of":  req.body.crime,
+        "Date_of_Conviction": req.body.doc,
+        "City_of_Conviction": req.body.city,
+        "Case_Number": req.body.case,
+        "Violent_Offense": req.body.violent,
+        "Time_in_State_Prison": req.body.statePris,
+        "Attend_Fire_Camp": req.body.fireCamp,
+        "Name_Fire_Camp": req.body.fireCampName,
+        "Fire_Camp_Start_Date": req.body.startFireCamp,
+        "Fire_Camp_End_Date": req.body.endFireCamp,
+        "Misdemeanor_or_Felony": req.body.misOrFel,
+        "Arrested_Since_Last_Conviction": req.body.arrested,
+        "Currently_on_Probation": req.body.probation,
+        "Comply_with_Probation_Terms": req.body.probTerms,
+        "Why_Expungement": req.body.whyExpunge,
+        "Employment_Situation": req.body.employment,
+        "Other_Convictions": req.body.otherConvic,
+        "Job-readiness/Job-training": req.body.jobTrain,
+        "Resume_Writing": req.body.resume,
+        "Social_Services_Help": req.body.socialServices,
+        "Additional_Notes": req.body.anythingElse,
+      }
+    })
 });
+
+
 
 
 app.get('/api/item/:itemId', (req: Request, res: Response) => {
