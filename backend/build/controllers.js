099119b10650b3abf992a44d59d746a144b588c3
19cmogush <cmogush@usc.edu>
2022-11-14 19:33:44 -0800

    fixing async issue

--- backend/build/controllers.js
+++ backend/build/controllers.js
@@ -15,59 +15,107 @@ exports.sendToAirtable = void 0;
  * @route POST /send-to-airtable
  */
 const sendToAirtable = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    /*
     const Airtable = require('airtable');
     const base = new Airtable({ apiKey: 'keyCIWaCgtovfq4fn' }).base('appiWMugRGpJ4p3KS');
     if (req.body.startFireCamp == '') {
-        req.body.startFireCamp = null;
-        req.body.endFireCamp = null;
+      req.body.startFireCamp = null;
+      req.body.endFireCamp = null;
     }
-    base('Table_1').create([
+    base('Table_1').create(
+      [
         {
-            fields: {
-                Full_Name: req.body.fullName,
-                Phone: req.body.phone,
-                Email: req.body.email,
-                Date_of_Birth: req.body.dob,
-                Crime_Convicted_of: req.body.crime,
-                Date_of_Conviction: req.body.doc,
-                City_of_Conviction: req.body.city,
-                Case_Number: req.body.case,
-                Violent_Offense: req.body.violent,
-                Time_in_State_Prison: req.body.statePris,
-                Attend_Fire_Camp: req.body.fireCamp,
-                Name_Fire_Camp: req.body.fireCampName,
-                Fire_Camp_Start_Date: req.body.startFireCamp,
-                Fire_Camp_End_Date: req.body.endFireCamp,
-                Misdemeanor_or_Felony: req.body.misOrFel,
-                Arrested_Since_Last_Conviction: req.body.arrested,
-                Currently_on_Probation: req.body.probation,
-                Comply_with_Probation_Terms: req.body.probTerms,
-                Why_Expungement: req.body.whyExpunge,
-                Employment_Situation: req.body.employment,
-                Other_Convictions: req.body.otherConvic,
-                'Job-readiness/Job-training': req.body.jobTrain,
-                Resume_Writing: req.body.resume,
-                Social_Services_Help: req.body.socialServices,
-                Additional_Notes: req.body.anythingElse
-            }
+          fields: {
+            Full_Name: req.body.fullName,
+            Phone: req.body.phone,
+            Email: req.body.email,
+            Date_of_Birth: req.body.dob,
+            Crime_Convicted_of: req.body.crime,
+            Date_of_Conviction: req.body.doc,
+            City_of_Conviction: req.body.city,
+            Case_Number: req.body.case,
+            Violent_Offense: req.body.violent,
+            Time_in_State_Prison: req.body.statePris,
+            Attend_Fire_Camp: req.body.fireCamp,
+            Name_Fire_Camp: req.body.fireCampName,
+            Fire_Camp_Start_Date: req.body.startFireCamp,
+            Fire_Camp_End_Date: req.body.endFireCamp,
+            Misdemeanor_or_Felony: req.body.misOrFel,
+            Arrested_Since_Last_Conviction: req.body.arrested,
+            Currently_on_Probation: req.body.probation,
+            Comply_with_Probation_Terms: req.body.probTerms,
+            Why_Expungement: req.body.whyExpunge,
+            Employment_Situation: req.body.employment,
+            Other_Convictions: req.body.otherConvic,
+            'Job-readiness/Job-training': req.body.jobTrain,
+            Resume_Writing: req.body.resume,
+            Social_Services_Help: req.body.socialServices,
+            Additional_Notes: req.body.anythingElse
+          }
         }
-    ], function (err, records) {
+      ],
+      function (err: any, records: any) {
         if (err) {
-            console.error(err);
-            return;
+          console.error(err);
+          return;
         }
-        records.forEach(function (record) {
-            console.log(record.getId());
+        records.forEach(function (record: any) {
+          console.log(record.getId());
         });
-    });
-    // export const updateAccount = async (req: Request, res: Response) => {
-    //   const user = req.user as User;
-    //   await user.update({
-    //     email: req.body.email,
-    //   });
-    //   return res.json(user);
-    // };
+      }
+    );
+    */
+    yield send(req);
     res.sendStatus(200);
 });
 exports.sendToAirtable = sendToAirtable;
+function send(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const Airtable = require('airtable');
+        const base = new Airtable({ apiKey: 'keyCIWaCgtovfq4fn' }).base('appiWMugRGpJ4p3KS');
+        if (req.body.startFireCamp == '') {
+            req.body.startFireCamp = null;
+            req.body.endFireCamp = null;
+        }
+        base('Table_1').create([
+            {
+                fields: {
+                    Full_Name: req.body.fullName,
+                    Phone: req.body.phone,
+                    Email: req.body.email,
+                    Date_of_Birth: req.body.dob,
+                    Crime_Convicted_of: req.body.crime,
+                    Date_of_Conviction: req.body.doc,
+                    City_of_Conviction: req.body.city,
+                    Case_Number: req.body.case,
+                    Violent_Offense: req.body.violent,
+                    Time_in_State_Prison: req.body.statePris,
+                    Attend_Fire_Camp: req.body.fireCamp,
+                    Name_Fire_Camp: req.body.fireCampName,
+                    Fire_Camp_Start_Date: req.body.startFireCamp,
+                    Fire_Camp_End_Date: req.body.endFireCamp,
+                    Misdemeanor_or_Felony: req.body.misOrFel,
+                    Arrested_Since_Last_Conviction: req.body.arrested,
+                    Currently_on_Probation: req.body.probation,
+                    Comply_with_Probation_Terms: req.body.probTerms,
+                    Why_Expungement: req.body.whyExpunge,
+                    Employment_Situation: req.body.employment,
+                    Other_Convictions: req.body.otherConvic,
+                    'Job-readiness/Job-training': req.body.jobTrain,
+                    Resume_Writing: req.body.resume,
+                    Social_Services_Help: req.body.socialServices,
+                    Additional_Notes: req.body.anythingElse
+                }
+            }
+        ], function (err, records) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            records.forEach(function (record) {
+                console.log(record.getId());
+            });
+        });
+    });
+}
 //# sourceMappingURL=controllers.js.map
--- backend/src/controllers.ts
+++ backend/src/controllers.ts
@@ -6,6 +6,7 @@ import Airtable, { Base } from 'airtable';
  * @route POST /send-to-airtable
  */
 export const sendToAirtable = async (req: Request, res: Response) => {
+  /*
   const Airtable = require('airtable');
   const base = new Airtable({ apiKey: 'keyCIWaCgtovfq4fn' }).base('appiWMugRGpJ4p3KS');
   if (req.body.startFireCamp == '') {
@@ -54,15 +55,59 @@ export const sendToAirtable = async (req: Request, res: Response) => {
       });
     }
   );
+  */
 
-  // export const updateAccount = async (req: Request, res: Response) => {
-  //   const user = req.user as User;
-
-  //   await user.update({
-  //     email: req.body.email,
-  //   });
-
-  //   return res.json(user);
-  // };
+  await send(req);
   res.sendStatus(200);
 };
+
+async function send(req: Request) {
+  const Airtable = require('airtable');
+  const base = new Airtable({ apiKey: 'keyCIWaCgtovfq4fn' }).base('appiWMugRGpJ4p3KS');
+  if (req.body.startFireCamp == '') {
+    req.body.startFireCamp = null;
+    req.body.endFireCamp = null;
+  }
+  base('Table_1').create(
+    [
+      {
+        fields: {
+          Full_Name: req.body.fullName,
+          Phone: req.body.phone,
+          Email: req.body.email,
+          Date_of_Birth: req.body.dob,
+          Crime_Convicted_of: req.body.crime,
+          Date_of_Conviction: req.body.doc,
+          City_of_Conviction: req.body.city,
+          Case_Number: req.body.case,
+          Violent_Offense: req.body.violent,
+          Time_in_State_Prison: req.body.statePris,
+          Attend_Fire_Camp: req.body.fireCamp,
+          Name_Fire_Camp: req.body.fireCampName,
+          Fire_Camp_Start_Date: req.body.startFireCamp,
+          Fire_Camp_End_Date: req.body.endFireCamp,
+          Misdemeanor_or_Felony: req.body.misOrFel,
+          Arrested_Since_Last_Conviction: req.body.arrested,
+          Currently_on_Probation: req.body.probation,
+          Comply_with_Probation_Terms: req.body.probTerms,
+          Why_Expungement: req.body.whyExpunge,
+          Employment_Situation: req.body.employment,
+          Other_Convictions: req.body.otherConvic,
+          'Job-readiness/Job-training': req.body.jobTrain,
+          Resume_Writing: req.body.resume,
+          Social_Services_Help: req.body.socialServices,
+          Additional_Notes: req.body.anythingElse
+        }
+      }
+    ],
+    function (err: any, records: any) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record: any) {
+        console.log(record.getId());
+      });
+    }
+  );
+}
